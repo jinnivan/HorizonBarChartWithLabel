091f04bcf16a355cd47bbf510667b1f0efdef783
jinnivan <jinnivan@msn.com>
2023-01-16 17:31:14 +0700

    Delete .tmp directory

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var bac49510ab1d3fa4399887df4f629646fbf: IVisualPlugin;
-export default bac49510ab1d3fa4399887df4f629646fbf;
