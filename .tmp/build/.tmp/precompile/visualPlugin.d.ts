c47c39234e6b8c9ae874df2de33340b211d59e89
jinnivan <jinnivan@msn.com>
2023-01-16 11:00:48 +0700

    Update

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -1,4 +1,4 @@
 import powerbiVisualsApi from "powerbi-visuals-api";
 import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var BAC49510ab1d3fa4399887df4f629646fbf_DEBUG: IVisualPlugin;
-export default BAC49510ab1d3fa4399887df4f629646fbf_DEBUG;
+declare var BAC49510ab1d3fa4399887df4f629646fbf: IVisualPlugin;
+export default BAC49510ab1d3fa4399887df4f629646fbf;
